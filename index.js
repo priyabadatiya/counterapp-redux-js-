58899ecec68f402fe5701bc277572261186f9d2e
priyabadatiya <priyabadatiya@gmail.com>
2021-12-02 12:31:12 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+let store=Redux.createStore(reducer)
+let counter=store.getState()
+    const h1 = document.querySelector('h1');
+    const increment = document.querySelector('.increment');
+    const decrement = document.querySelector('.decrement');
+    const reset = document.querySelector('.reset');
+
+    h1.innerText = counter;
+
+    increment.addEventListener('click', () => {
+        store.dispatch({type:'increment'})
+    });
+
+    decrement.addEventListener('click', () => {
+        store.dispatch({type:'decrement'})
+    });
+
+    reset.addEventListener('click', () => {
+        store.dispatch({type:'reset'});
+    })
+    store.subscribe(()=>{
+        counter = store.getState();
+        h1.innerText=counter;
+    })
+
+function reducer(state = 0, action) {
+    switch (action.type) {
+        case 'increment':
+            return state + (action.step || 1)
+        case 'decrement':
+            return state - (action.step || 1)
+        case 'reset':
+            return 0
+
+            default:
+               return state;
+    }
+}
